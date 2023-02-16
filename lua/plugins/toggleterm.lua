ac7826e286df863064b441c27ee9bcb9cdcd71c7
Knosence <nadario.seays@gmail.com>
2023-02-16 13:52:30 -0500

    Absolutely works now. Please no one breath!!

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,70 +1,35 @@
 return {
-require('toggleterm').setup({
+    'akinsho/toggleterm.nvim',
+    config = function()
+	require('toggleterm').setup{
 
-    config = function ()
-    Size = 10
-    Open_mapping = [[<c-\>]]
-    Hide_numbers = true
-    Shade_filetypes = {}
-    Shade_terminals = true
-    Shading_factor = 2
-    Start_in_insert = true
-    Insert_mappings = true
-    Persist_size = true
-    Direction = "horizontal"
-    Close_on_exit = true
-    Shell = vim.o.shell
-    Float_opts = {
-	border = "curved",
-	winblend = 0,
-	highlights = {
-	    border = "Normal",
-	    background = "Normal",
-	},
-    }
+	    function(term)
+		if term.direction == "horizontal" then
+		    return 15
+		elseif term.direction == "vertical" then
+		    return vim.o.columns * 0.4
+		end
+	    end,
 
-
-function _G.set_terminal_keymaps()
-    local opts = {noremap = true}
-    vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', 'kj', [[<C-\><C-n>]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-    vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-end
-
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-end
-    }
-)}
--- local Terminal = require("toggleterm.terminal").Terminal
--- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-
--- function _LAZYGIT_TOGGLE()
---     lazygit:toggle()
--- end
---
--- local node = Terminal:new({ cmd = "node", hidden = true })
---
--- function _NODE_TOGGLE()
---     node:toggle()
--- end
---
--- local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
---
--- function _NCDU_TOGGLE()
---     ncdu:toggle()
--- end
---
--- local htop = Terminal:new({ cmd = "htop", hidden = true })
---
--- function _HTOP_TOGGLE()
---     htop:toggle()
--- end
---
--- local python = Terminal:new({ cmd = "python", hidden = true })
---
--- function _PYTHON_TOGGLE()
---     python:toggle()
--- end
+	    open_mapping = [[<C-\>]],
+	    hide_number = false,
+	    shade_filetypes = {},
+	    shade_terminals = true,
+	    start_in_insert = true,
+	    insert_mappings = true,
+	    presist_size = true,
+	    -- direction = 'vertical' 'horizontal' 'window' 'float'
+	    direction = 'horizontal',
+	    close_on_exit = true,
+	    float_opts = {
+		-- border = 'single' 'double' 'shadow' 'curved'
+		boarder = 'curved',
+		winblend = 3,
+		highlights = {
+		    border = "Normal",
+		    background = "Normal"
+		}
+	    }
+	}
+    end
+}
