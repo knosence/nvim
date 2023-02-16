8cdefa3f3ffc1b2e464200b6e135e753325c431e
Knosence <nadario.seays@gmail.com>
2023-02-16 15:50:49 -0500

    autopilot somewhat works, need to work on some more

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -8,3 +8,5 @@ vim.keymap.set('n', '<C-k>', '<C-w>k', {silent = true})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {silent = true})
 vim.keymap.set('n', '<C-l>', '<C-w>l', {silent = true})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {silent = true})
+
+
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -31,5 +31,11 @@ return {
 		}
 	    }
 	}
-    end
+    end,
+
+    vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], {noremap = true, silent = true}),
+    vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], {noremap = true, silent = true}),
+    vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], {noremap = true, silent = true}),
+    vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], {noremap = true, silent = true}),
+    vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], {noremap = true, silent = true}),
 }
