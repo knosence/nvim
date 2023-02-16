d2c7bb1e76de560238329695220c8c5f959eca13
Knosence <nadario.seays@gmail.com>
2023-02-16 10:23:24 -0500

    added new config starting point

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  'nvim-lualine/lualine.nvim', -- Fancier statusline
+
+  config = function()
+
+    -- Set lualine as statusline
+    -- See `:help lualine.txt`
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+    }
+
+  end
+}
